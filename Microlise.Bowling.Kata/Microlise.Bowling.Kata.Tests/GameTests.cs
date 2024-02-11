e99d6e899f3f802fad90bf894637d65676a48a40
Dante <db470@student.le.ac.uk>
2024-02-11 14:40:20 +0000

    Game class and unit tests

---
+++ Microlise.Bowling.Kata/Microlise.Bowling.Kata.Tests/GameTests.cs
@@ -0,0 +1,62 @@
+using System.Net.NetworkInformation;
+using System.Runtime.ExceptionServices;
+
+namespace Microlise.Bowling.Kata.Tests
+{
+    public class Tests
+    {
+        private Game _game;
+        [SetUp]
+        public void Setup()
+        {
+            _game = new Game();
+        }
+
+        [Test]
+        public void ANewGame_should_returnZero()
+        {
+            Assert.That(_game.Score, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Game_AterRollingTwoBallsKnockingOverOnePin_ReturnScoreOfTwo()
+        {
+            _game.RollBall(1);
+            _game.RollBall(1);
+            Assert.That(_game.Score, Is.EqualTo(2));
+        }
+
+        [TestCase(20, 0, 0)]
+        [TestCase(2, 1, 2)]
+        [TestCase(20, 3, 60)]
+        public void Game_RollBallKnockingOverPins_ReturnCorrectScore(int numberOfRolls, int pins, int expected)
+        {
+            RollMany(numberOfRolls, pins);
+
+            Assert.That(_game.Score, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Game_AfterRollingSpare_ReturnsCorrectScore()
+        {
+            _game.RollBall(5);
+            _game.RollBall(5);
+            _game.RollBall(3);
+            Assert.That(_game.Score, Is.EqualTo(16));
+        }
+
+        [Test]
+        public void Game_3()
+        {
+            Assert.Pass();
+        }
+
+        private void RollMany(int ballsRolled, int pinsKnockedOver)
+        {
+            for (int i = 0; i < ballsRolled; i++)
+            {
+                _game.RollBall(pinsKnockedOver);
+            }
+        }
+    }
+}
---
+++ Microlise.Bowling.Kata/Microlise.Bowling.Kata/Game.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microlise.Bowling.Kata
+{
+    public class Game
+    {
+        private readonly int[] _knockedOverPins = new int[21];
+        private int _currentRoll;
+        public int Score()
+        {
+            int score = 0;
+
+            for (var roll = 0; roll < _currentRoll; roll += 2)
+            {
+                int frameScore = _knockedOverPins[roll] + _knockedOverPins[roll + 1];
+                if (frameScore == 10)
+                {
+                    score += _knockedOverPins[roll + 2];
+                }
+
+                score += frameScore;
+            }
+
+            return score;
+        }
+
+        public void RollBall(int pins)
+        {
+            _knockedOverPins[_currentRoll] = pins;
+            _currentRoll++;
+        }
+    }
+}
